ee1eb7591802f6ff8fbee09740938ba5a58f9a40
DikDns <dikdnssocial@gmail.com>
2023-06-26 12:39:43 +0700

    docs: get IGDB token

--- src/controllers/getIGDBToken.ts
+++ src/controllers/getIGDBToken.ts
@@ -1,6 +1,10 @@
 import { cache } from "react";
 import { prisma } from "@/lib/db";
 
+/**
+ * Retrieves the IGDB token from the Prisma database.
+ * @returns A promise that resolves with the IGDB token.
+ */
 async function getIGDBToken() {
   const igdbToken = await prisma.iGDBToken.findMany();
   return igdbToken;
