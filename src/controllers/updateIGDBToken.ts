a78a0412a2e4cbdf3807d441dd366b670d534617
DikDns <dikdnssocial@gmail.com>
2023-06-26 12:40:39 +0700

    docs: update IGDB token

--- src/controllers/updateIGDBToken.ts
+++ src/controllers/updateIGDBToken.ts
@@ -1,5 +1,13 @@
 import { prisma } from "@/lib/db";
 
+/**
+ * Updates the IGDB token in the Prisma database.
+ * @param currentAccessToken - The current access token to be updated.
+ * @param newAccessToken - The new access token value.
+ * @param expiredAt - The new expiration date of the access token.
+ * @returns A promise that resolves when the token is successfully updated.
+ * @throws An error if the token update fails.
+ */
 export default async function updateIGDBToken(
   currentAccessToken: string,
   newAccessToken: string,
