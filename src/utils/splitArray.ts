b5e3210740b0a5424195692c0ee382f97e16e1cd
DikDns <dikdnssocial@gmail.com>
2023-06-28 13:05:41 +0700

    feat: add splitArray method

---
+++ src/utils/splitArray.ts
@@ -0,0 +1,17 @@
+/**
+ * Splits an array into smaller arrays of a given length.
+ * @param array - The array to be split.
+ * @param chunkSize - The desired length of each subarray.
+ * @returns An array of smaller arrays, where each subarray has a length of chunkSize.
+ */
+
+function splitArray<T>(array: T[], chunkSize: number): T[][] {
+  const splitArrays: T[][] = [];
+
+  for (let i = 0; i < array.length; i += chunkSize) {
+    const chunk = array.slice(i, i + chunkSize);
+    splitArrays.push(chunk);
+  }
+
+  return splitArrays;
+}
