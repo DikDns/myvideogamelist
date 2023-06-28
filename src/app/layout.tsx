1e3b32e7f12303f2e5f199eb56cd0c1fadd78190
DikDns <dikdnssocial@gmail.com>
2023-06-28 10:16:26 +0700

    feat: add material ui provider to root layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,11 @@
-import "./globals.css";
-import { Inter } from "next/font/google";
-import NextAuthSessionProvider from "@/providers/NextAuthSessionProvider";
-import ChakraUIProvider from "@/providers/ChakraUIProvider";
-
-const inter = Inter({ subsets: ["latin"] });
+import * as React from "react";
+import ThemeRegistry from "@/components/Theme/ThemeRegistry";
+import SessionProvider from "@/components/NextAuth/SessionProvider";
 
 export const metadata = {
   title: "MyVideoGameList",
   description:
-    "Welcome to MyVideoGameList, the world's most active online video games community and database. Join the online community, create your video game list.",
+    "Welcome to MyVideoGameList, the world's most active online video games community and database. Join the online community, create your video game list. Now!",
 };
 
 export default function RootLayout({
@@ -18,10 +15,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <NextAuthSessionProvider>
-          <ChakraUIProvider>{children}</ChakraUIProvider>
-        </NextAuthSessionProvider>
+      <body>
+        <SessionProvider>
+          <ThemeRegistry>{children}</ThemeRegistry>
+        </SessionProvider>
       </body>
     </html>
   );
