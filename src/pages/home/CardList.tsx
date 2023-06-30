b343383863147df1b4a32e6702490cac8d11471c
DikDns <dikdnssocial@gmail.com>
2023-06-30 08:24:57 +0700

    feat: add card hover animation

--- src/pages/home/CardList.tsx
+++ src/pages/home/CardList.tsx
@@ -3,11 +3,27 @@
 import Image from "next/image";
 import { NewReleaseData } from "./NewReleases";
 import CardItem from "./CardItem";
+import Box from "@mui/material/Box";
 
 export default function CardList({ data }: { data: NewReleaseData[] }) {
   return data.map((dt) => (
     <CardItem key={dt.id}>
-      <div style={{ position: "relative", width: "100%", height: "100%" }}>
+      <Box
+        sx={{
+          position: "relative",
+          width: "100%",
+          height: "100%",
+          cursor: "pointer",
+          "& img": {
+            transition: "all 250ms ease-in-out",
+            filter: "brightness(75%)",
+          },
+          "&:hover img": {
+            transform: "scale(1.05)",
+            filter: "brightness(100%)",
+          },
+        }}
+      >
         <Image
           style={{ objectFit: "cover", objectPosition: "center" }}
           src={`https://images.igdb.com/igdb/image/upload/t_cover_big/${dt.cover.image_id}.jpg`}
@@ -16,7 +32,7 @@ export default function CardList({ data }: { data: NewReleaseData[] }) {
           fill={true}
           sizes="(max-width: 768px) 264px, 200px"
         />
-      </div>
+      </Box>
     </CardItem>
   ));
 }
