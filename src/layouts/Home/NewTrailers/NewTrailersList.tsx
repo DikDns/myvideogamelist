28b7a358560975f360b997d5d18714ee86a3db5b
DikDns <dikdnssocial@gmail.com>
2023-07-01 10:47:20 +0700

    feat: add default style to newtrailers list layout

--- src/layouts/Home/NewTrailers/NewTrailersList.tsx
+++ src/layouts/Home/NewTrailers/NewTrailersList.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import Box from "@mui/material/Box";
-import YoutubeIframe from "@/components/YoutubeIframe/YoutubeIframe";
+import Skeleton from "@mui/material/Skeleton";
+import YoutubePlayer from "@/components/VideoPlayer/YoutubePlayer";
 import { NewTrailersData } from "./NewTrailersInterface";
 
 const newTrailerItemStyle = {
@@ -9,18 +10,19 @@ const newTrailerItemStyle = {
   flex: "0 0 auto",
   minWidth: "0",
   maxWidth: "100%",
-  width: "400px",
-  height: "225px",
+  overflow: "hidden",
+  boxShadow: 2,
+  borderRadius: "5px",
 };
 
 export default function NewTrailersList({ data }: { data: NewTrailersData[] }) {
   return data.map((dt) => (
-    <Box sx={newTrailerItemStyle}>
-      <YoutubeIframe
+    <Box sx={newTrailerItemStyle} key={dt.id}>
+      <YoutubePlayer
         videoId={dt.video_id}
-        title={`${dt.game.name} ${dt.name}`}
-        key={dt.id}
-        styles={{ width: "100%", height: "100%" }}
+        width="100%"
+        height="95%"
+        fallback={<Skeleton variant="rectangular" width="95%" height="100%" />}
       />
     </Box>
   ));
