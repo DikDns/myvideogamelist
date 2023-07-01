b769252440af2858c9e6703fcc7eb89348a34d0d
DikDns <dikdnssocial@gmail.com>
2023-07-01 08:20:07 +0700

    fix: font export

--- src/app/fonts/fonts.ts
+++ src/app/fonts/fonts.ts
@@ -3,4 +3,4 @@ import { Raleway, Open_Sans } from "next/font/google";
 const GoogleRalewayFont = Raleway({ subsets: ["latin"] });
 const GoogleOpenSansFont = Open_Sans({ subsets: ["latin"] });
 
-export default { GoogleRalewayFont, GoogleOpenSansFont };
+export { GoogleRalewayFont, GoogleOpenSansFont };
