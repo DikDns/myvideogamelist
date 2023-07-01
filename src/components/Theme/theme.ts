b1eb6caaf97e978698d16ebb344e38c1f25bba13
DikDns <dikdnssocial@gmail.com>
2023-07-01 08:21:16 +0700

    feat: add custom palette mvgl brand

--- src/components/Theme/theme.ts
+++ src/components/Theme/theme.ts
@@ -1,16 +1,40 @@
 "use client";
 
 import { createTheme } from "@mui/material/styles";
-import GoogleInterFont from "@/app/fonts/fonts";
+import { GoogleOpenSansFont, GoogleRalewayFont } from "@/app/fonts/fonts";
 
 const defaultTheme = createTheme({
   palette: {
     mode: "dark",
+    primary: {
+      main: "#ef6a00",
+    },
+    secondary: {
+      main: "#00ef55",
+    },
   },
   typography: {
-    fontFamily: GoogleInterFont.style.fontFamily,
-    body1: { fontFamily: GoogleInterFont.style.fontFamily },
-    body2: { fontFamily: GoogleInterFont.style.fontFamily },
+    fontFamily: GoogleOpenSansFont.style.fontFamily,
+    h1: {
+      fontFamily: GoogleRalewayFont.style.fontFamily,
+    },
+    h2: {
+      fontFamily: GoogleRalewayFont.style.fontFamily,
+    },
+    h3: {
+      fontFamily: GoogleRalewayFont.style.fontFamily,
+    },
+    h4: {
+      fontFamily: GoogleRalewayFont.style.fontFamily,
+    },
+    h5: {
+      fontFamily: GoogleRalewayFont.style.fontFamily,
+    },
+    h6: {
+      fontFamily: GoogleRalewayFont.style.fontFamily,
+    },
+    body1: { fontFamily: GoogleOpenSansFont.style.fontFamily },
+    body2: { fontFamily: GoogleOpenSansFont.style.fontFamily },
   },
 });
 
