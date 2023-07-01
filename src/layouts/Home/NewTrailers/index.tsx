c696bdc03fc39c6c5da40134e4c707a7c5169a6f
DikDns <dikdnssocial@gmail.com>
2023-07-01 10:46:53 +0700

    add: NewTrailers section index

---
+++ src/layouts/Home/NewTrailers/index.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import VideoCarousel from "@/components/Carousel/VideoCarousel";
+import NewTrailersList from "./NewTrailersList";
+import { NewTrailersData } from "./NewTrailersInterface";
+
+const h3Style = {
+  fontSize: {
+    xs: "1.25rem",
+    sm: "2rem",
+    md: "2.5rem",
+  },
+};
+
+export default function NewTrailers({ data }: { data: NewTrailersData[] }) {
+  return (
+    <Container sx={{ overflow: "hidden", my: 10 }}>
+      <Typography sx={h3Style} variant="h3" mb={2}>
+        {`New Trailers`}
+      </Typography>
+      <VideoCarousel>
+        <NewTrailersList data={data} />
+      </VideoCarousel>
+    </Container>
+  );
+}
