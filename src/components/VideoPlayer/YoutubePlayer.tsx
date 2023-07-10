0b77950fcaa3b775800f3938d409a8995b62dfba
DikDns <dikdnssocial@gmail.com>
2023-07-10 10:06:00 +0700

    feat: add play icon mui in YoutubePlayer

--- src/components/VideoPlayer/YoutubePlayer.tsx
+++ src/components/VideoPlayer/YoutubePlayer.tsx
@@ -3,6 +3,7 @@
 import dynamic from "next/dynamic";
 import { BaseReactPlayerProps } from "react-player/base";
 const ReactPlayer = dynamic(() => import("react-player/lazy"), { ssr: false });
+import PlayCircle from "@mui/icons-material/PlayCircleFilledTwoTone";
 
 interface YoutubePlayer extends BaseReactPlayerProps {
   videoId: string;
@@ -32,6 +33,7 @@ export default function YoutubePlayer({
       width={width}
       height={height}
       fallback={fallback}
+      playIcon={<PlayCircle fontSize="large" className="play-icon" />}
     />
   );
 }
