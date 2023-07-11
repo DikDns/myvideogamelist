69eeeec572a829904fdd76ca48e12d7fc2884b73
DikDns <dikdnssocial@gmail.com>
2023-07-11 09:07:23 +0700

    add HomeData types import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,9 @@
+import getHomeData from "./server/getHomeData";
 import Home from "@/layouts/Home";
+import { HomeData } from "@/types/HomeDataType";
 
 export default async function IndexPage() {
-  return <Home />;
+  const data: HomeData = await getHomeData();
+
+  return <Home data={data} />;
 }
