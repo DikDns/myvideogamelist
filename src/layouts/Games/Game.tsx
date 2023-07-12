be2edace361522e9f94f0a742b6bd8f6f23cc894
DikDns <dikdnssocial@gmail.com>
2023-07-12 09:22:13 +0700

    add Game Layout intial client component

---
+++ src/layouts/Games/Game.tsx
@@ -0,0 +1,13 @@
+import { VideoGame } from "@/types/VideoGameType";
+
+import Container from "@mui/material/Container";
+import { useEffect } from "react";
+
+export default function Game({ data }: { data: VideoGame[] }) {
+  useEffect(() => {
+    console.log(data);
+  }, []);
+  return (
+    <Container component="main" sx={{ overflow: "hidden", my: 8 }}></Container>
+  );
+}
