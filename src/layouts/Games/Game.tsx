953927a99b2866ca7bbad11ace4fd9985a03a45c
DikDns <dikdnssocial@gmail.com>
2023-07-12 13:23:13 +0700

    add cover in game page

--- src/layouts/Games/Game.tsx
+++ src/layouts/Games/Game.tsx
@@ -1,17 +1,25 @@
 "use client";
 
+import { useEffect } from "react";
 import { VideoGame } from "@/types/VideoGameType";
 import Container from "@mui/material/Container";
-import { useEffect } from "react";
+import Image from "next/image";
 import AgeRatingImages from "./components/AgeRatingImages";
+import { getImageUrl } from "@/lib/igdb";
 
-export default function Game({ data }: { data: VideoGame[] }) {
+export default function Game({ data }: { data: VideoGame }) {
   useEffect(() => {
     console.log(data);
   }, []);
   return (
     <Container component="main" sx={{ overflow: "hidden", my: 8 }}>
-      <AgeRatingImages ageRatings={data[0].age_ratings || []} />
+      <Image
+        src={getImageUrl(data.cover?.image_id || "", "cover_small")}
+        alt={`${data.name} Cover`}
+        width={90}
+        height={128}
+      />
+      <AgeRatingImages ageRatings={data.age_ratings || []} />
     </Container>
   );
 }
