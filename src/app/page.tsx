5f0d663337c7e7d20ec960c1f979b26e197b1243
DikDns <dikdnssocial@gmail.com>
2023-07-12 07:32:21 +0700

    remove HomeData type in App/page home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
 import getHomeData from "./server/getHomeData";
 import Home from "@/layouts/Home";
-import { HomeData } from "@/types/HomeDataType";
 
 export default async function IndexPage() {
-  const data: HomeData = await getHomeData();
+  const data = await getHomeData();
 
   return <Home data={data} />;
 }
