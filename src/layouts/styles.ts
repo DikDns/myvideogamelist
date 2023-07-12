4be725325abadb03b475880754ff9d01e3e4c80c
DikDns <dikdnssocial@gmail.com>
2023-07-13 06:42:21 +0700

    move styles.ts to layout

---
+++ src/layouts/styles.ts
@@ -0,0 +1,49 @@
+import { SxProps } from "@mui/material/styles";
+
+export const h1: SxProps = {
+  fontSize: {
+    xs: "2rem",
+    sm: "3rem",
+    md: "3.5rem",
+  },
+};
+
+export const h2: SxProps = {
+  fontSize: {
+    xs: "1.5rem",
+    sm: "2.5rem",
+    md: "3rem",
+  },
+};
+
+export const h3: SxProps = {
+  fontSize: {
+    xs: "1.25rem",
+    sm: "2rem",
+    md: "2.5rem",
+  },
+};
+
+export const h4: SxProps = {
+  fontSize: {
+    xs: "1.25rem",
+    sm: "1.5rem",
+    md: "2rem",
+  },
+};
+
+export const h5: SxProps = {
+  fontSize: {
+    xs: "1.25rem",
+    sm: "1.5rem",
+    md: "2rem",
+  },
+};
+
+export const h6: SxProps = {
+  fontSize: {
+    xs: "0.75rem",
+    sm: "1.25rem",
+    md: "1.5rem",
+  },
+};
