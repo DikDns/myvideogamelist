ccd6ea87a682c765087cc308827d9cf4fa8e85a6
DikDns <dikdnssocial@gmail.com>
2023-07-12 08:48:55 +0700

    add index metada title template

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,16 @@ import ThemeRegistry from "@/components/Theme/ThemeRegistry";
 import SessionProvider from "@/components/NextAuth/SessionProvider";
 
 export const metadata = {
-  title: "MyVideoGameList",
+  referrer: "origin-when-cross-origin",
+  keywords: ["MVGL", "My Video Game List", "Video Game Tracker"],
+  authors: [{ name: "DikDns", url: "https://linktr.ee/dikdns" }],
+  colorScheme: "dark",
+  creator: "DikDns",
+  publisher: "DikDns",
+  title: {
+    template: "%s | MVGL",
+    default: "MyVideoGameList",
+  },
   description:
     "Welcome to MyVideoGameList, the world's most active online video games community and database. Join the online community, create your video game list. Now!",
 };
