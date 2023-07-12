d6d73060b52ab716ce19066131727bce44d98a15
DikDns <dikdnssocial@gmail.com>
2023-07-12 11:04:47 +0700

    add AgeRating image list component

--- src/layouts/Games/Game.tsx
+++ src/layouts/Games/Game.tsx
@@ -3,12 +3,15 @@
 import { VideoGame } from "@/types/VideoGameType";
 import Container from "@mui/material/Container";
 import { useEffect } from "react";
+import AgeRatingImages from "./components/AgeRatingImages";
 
 export default function Game({ data }: { data: VideoGame[] }) {
   useEffect(() => {
     console.log(data);
   }, []);
   return (
-    <Container component="main" sx={{ overflow: "hidden", my: 8 }}></Container>
+    <Container component="main" sx={{ overflow: "hidden", my: 8 }}>
+      <AgeRatingImages ageRatings={data[0].age_ratings || []} />
+    </Container>
   );
 }
---
+++ src/layouts/Games/components/AgeRatingImages.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image";
+import { getAgeRating } from "@/lib/igdb";
+import { AgeRating } from "@/types/AgeRating";
+
+function AgeRatingImage({ url, title }: { url: string; title: string }) {
+  return <Image src={url} alt={title} width={100} height={100} />;
+}
+
+export default function AgeRatingImages({
+  ageRatings,
+}: {
+  ageRatings: AgeRating[];
+}) {
+  return ageRatings.map((ageRating) => {
+    if (!ageRating.rating) return;
+    const { url, title } = getAgeRating(ageRating.rating);
+    return <AgeRatingImage url={url} title={title} key={ageRating.id} />;
+  });
+}
