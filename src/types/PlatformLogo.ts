7d74f871f48e3feb2575d2ec2b094c75b58f802d
DikDns <dikdnssocial@gmail.com>
2023-07-13 09:21:16 +0700

    add PlatformLogo Type

---
+++ src/types/PlatformLogo.ts
@@ -0,0 +1,10 @@
+export type PlatformLogo = {
+  id: number;
+  alpha_channel?: boolean;
+  animated?: boolean;
+  checksum?: string;
+  height?: number;
+  image_id?: string;
+  url?: string;
+  width?: number;
+};
