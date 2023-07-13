098b731504bfee1d9b93f1cd69af6bf576d3bd0e
DikDns <dikdnssocial@gmail.com>
2023-07-13 08:48:18 +0700

    refactor Display Name Cover to GameHeading Component

--- src/layouts/Games/Game.tsx
+++ src/layouts/Games/Game.tsx
@@ -1,16 +1,10 @@
 "use client";
 
 import { useEffect } from "react";
-import Image from "next/image";
 import { Game } from "@/types/Game";
-import { getImageUrl } from "@/lib/igdb";
 import Container from "@mui/material/Container";
-import Typography from "@mui/material/Typography";
-import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
 import AgeRatingImages from "./components/AgeRatingImages";
-import { h2, h3 } from "../styles";
-import formatUnix from "@/utils/formatUnix";
+import GameHeading from "./components/GameHeading";
 
 export default function Game({ game }: { game: Game }) {
   useEffect(() => {
@@ -19,53 +13,8 @@ export default function Game({ game }: { game: Game }) {
 
   return (
     <>
-      <Box
-        pt={8}
-        sx={{
-          backgroundSize: "cover",
-          backgroundPosition: "center",
-          backgroundImage: `url(${getImageUrl(
-            // @ts-ignore
-            game.screenshots[0].image_id || "",
-            "screenshot_med"
-          )});`,
-        }}
-      >
-        <Grid
-          alignItems="center"
-          flexWrap="nowrap"
-          container
-          pt={2}
-          pb={1}
-          px={2}
-          gap={2}
-          sx={{
-            backgroundColor: "#121212b2",
-            backdropFilter: "blur(4px)",
-          }}
-        >
-          <Grid item sx={{ borderRadius: "2px", overflow: "hidden" }} xs="auto">
-            <Image
-              src={getImageUrl(game.cover?.image_id || "", "cover_small")}
-              alt={`${game.name} Cover`}
-              width={90}
-              height={128}
-            />
-          </Grid>
-          <Grid container item direction="column" xs={10} gap={1}>
-            <Grid item>
-              <Typography sx={h2} variant="h2">
-                {game.name}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography sx={h3} variant="h3">
-                {`${formatUnix(game.first_release_date || 0)}`}
-              </Typography>
-            </Grid>
-          </Grid>
-        </Grid>
-      </Box>
+      {/* Heading: Game Cover and Name */}
+      <GameHeading game={game} />
 
       <Container component="main" sx={{ overflow: "hidden", my: 8 }}>
         <AgeRatingImages ageRatings={game.age_ratings || []} />
---
+++ src/layouts/Games/components/GameHeading.tsx
@@ -0,0 +1,60 @@
+import Image from "next/image";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import { h2, h3 } from "@/layouts/styles";
+import { getImageUrl } from "@/lib/igdb";
+import formatUnix from "@/utils/formatUnix";
+import { Game } from "@/types/Game";
+
+export default function GameHeading({ game }: { game: Game }) {
+  return (
+    <Box
+      pt={8}
+      sx={{
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundImage: `url(${getImageUrl(
+          // @ts-ignore
+          game.screenshots[0].image_id || "",
+          "screenshot_med"
+        )});`,
+      }}
+    >
+      <Grid
+        alignItems="center"
+        flexWrap="nowrap"
+        container
+        pt={2}
+        pb={1}
+        px={2}
+        gap={2}
+        sx={{
+          backgroundColor: "#121212b2",
+          backdropFilter: "blur(4px)",
+        }}
+      >
+        <Grid item sx={{ borderRadius: "2px", overflow: "hidden" }} xs="auto">
+          <Image
+            src={getImageUrl(game.cover?.image_id || "", "cover_small")}
+            alt={`${game.name} Cover`}
+            width={90}
+            height={128}
+          />
+        </Grid>
+        <Grid container item direction="column" xs={10} gap={1}>
+          <Grid item>
+            <Typography sx={h2} variant="h2">
+              {game.name}
+            </Typography>
+          </Grid>
+          <Grid item>
+            <Typography sx={h3} variant="h3">
+              {`${formatUnix(game.first_release_date || 0)}`}
+            </Typography>
+          </Grid>
+        </Grid>
+      </Grid>
+    </Box>
+  );
+}
