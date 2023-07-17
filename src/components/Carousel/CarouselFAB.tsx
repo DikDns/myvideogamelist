34e09a05d0aa08db4682cc239c6e561510c92f13
DikDns <dikdnssocial@gmail.com>
2023-07-17 09:21:40 +0700

    add CarouselFAB component

---
+++ src/components/Carousel/CarouselFAB.tsx
@@ -0,0 +1,52 @@
+import { useCallback } from "react";
+import KeyboardArrowLeftRounded from "@mui/icons-material/KeyboardArrowLeftRounded";
+import KeyboardArrowRightRounded from "@mui/icons-material/KeyboardArrowRightRounded";
+import Fab from "@mui/material/Fab";
+import Grow from "@mui/material/Grow";
+import { EmblaCarouselType } from "embla-carousel-react";
+
+export default function CardCarouselFAB({
+  isMobile,
+  isHovered,
+  emblaApi,
+}: {
+  isMobile: boolean;
+  isHovered: boolean;
+  emblaApi: EmblaCarouselType | undefined;
+}) {
+  if (!emblaApi || isMobile) return;
+
+  const scrollPrev = useCallback(() => {
+    emblaApi.scrollPrev();
+  }, [emblaApi]);
+
+  const scrollNext = useCallback(() => {
+    emblaApi.scrollNext();
+  }, [emblaApi]);
+
+  return (
+    <>
+      <Grow in={isHovered}>
+        <Fab
+          aria-label="previous"
+          variant="circular"
+          onClick={scrollPrev}
+          sx={{ position: "absolute", left: "2%", top: "42%" }}
+        >
+          <KeyboardArrowLeftRounded fontSize="medium" />
+        </Fab>
+      </Grow>
+      <Grow in={isHovered}>
+        <Fab
+          aria-label="next"
+          variant="circular"
+          color="default"
+          onClick={scrollNext}
+          sx={{ position: "absolute", right: "2%", top: "42%" }}
+        >
+          <KeyboardArrowRightRounded fontSize="medium" />
+        </Fab>
+      </Grow>
+    </>
+  );
+}
