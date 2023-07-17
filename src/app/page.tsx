4304facd22db556c95dce1300522eff7a1cfd3dc
DikDns <dikdnssocial@gmail.com>
2023-07-17 11:51:56 +0700

    rename home to hahaha

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import getHomeData from "./server/getHomeData";
-import Home from "@/layouts/Home/home";
+import Home from "@/layouts/Home/hahaha";
 
 export default async function IndexPage() {
   const data = await getHomeData();
