9251bd5e3eef52f450b9985d3f9cba9a627a5d45
DikDns <dikdnssocial@gmail.com>
2023-07-17 11:06:46 +0700

    add CarouselFAB flex alignment

--- src/components/Carousel/CarouselFAB.tsx
+++ src/components/Carousel/CarouselFAB.tsx
@@ -1,9 +1,22 @@
 import { useCallback } from "react";
 import KeyboardArrowLeftRounded from "@mui/icons-material/KeyboardArrowLeftRounded";
 import KeyboardArrowRightRounded from "@mui/icons-material/KeyboardArrowRightRounded";
+import Box from "@mui/material/Box";
 import Fab from "@mui/material/Fab";
 import Grow from "@mui/material/Grow";
 import { EmblaCarouselType } from "embla-carousel-react";
+
+const ContainerFABStyle = {
+  pointerEvents: "none",
+  height: "100%",
+  width: "100%",
+  px: "8px",
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "space-between",
+  position: "absolute",
+  top: 0,
+};
 
 export default function CardCarouselFAB({
   isMobile,
@@ -25,28 +38,32 @@ export default function CardCarouselFAB({
   }, [emblaApi]);
 
   return (
-    <>
+    <Box sx={ContainerFABStyle}>
       <Grow in={isHovered}>
         <Fab
+          sx={{
+            pointerEvents: "auto",
+          }}
           aria-label="previous"
           variant="circular"
           onClick={scrollPrev}
-          sx={{ position: "absolute", left: "2%", top: "42%" }}
         >
           <KeyboardArrowLeftRounded fontSize="medium" />
         </Fab>
       </Grow>
       <Grow in={isHovered}>
         <Fab
+          sx={{
+            pointerEvents: "auto",
+          }}
           aria-label="next"
           variant="circular"
           color="default"
           onClick={scrollNext}
-          sx={{ position: "absolute", right: "2%", top: "42%" }}
         >
           <KeyboardArrowRightRounded fontSize="medium" />
         </Fab>
       </Grow>
-    </>
+    </Box>
   );
 }
