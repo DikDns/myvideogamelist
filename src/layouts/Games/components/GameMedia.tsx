458a86ea18bafaf1be5d04c9400e098e266b71b5
DikDns <dikdnssocial@gmail.com>
2023-07-17 14:06:12 +0700

    add GameMedia component

---
+++ src/layouts/Games/components/GameMedia.tsx
@@ -0,0 +1,20 @@
+"use client";
+import MediaCarousel from "@/components/Carousel/MediaCarousel";
+import ScreenshotSlide from "./ScreenshotSlide";
+import { Game } from "@/types/Game";
+import VideoSlide from "./VideoSlide";
+
+export default function GameMedia({ game }: { game: Game }) {
+  return game.screenshots && game.videos ? (
+    <MediaCarousel
+      slides={[
+        ...game.videos.map((video) => (
+          <VideoSlide video={video} key={video.id} />
+        )),
+        ...game.screenshots.map((screenshot) => (
+          <ScreenshotSlide screenshot={screenshot} key={screenshot.id} />
+        )),
+      ]}
+    />
+  ) : undefined;
+}
