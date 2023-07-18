98381427261d905a5a86c9da5447a3f627b82f2f
DikDns <dikdnssocial@gmail.com>
2023-07-18 15:33:57 +0700

    add companies field query in Game Page

--- src/app/games/[slug]/page.tsx
+++ src/app/games/[slug]/page.tsx
@@ -28,14 +28,24 @@ export async function generateMetadata(
 
 export default async function GamePage({ params: { slug } }: GamePageProps) {
   const body = `
-    f *, cover.image_id, 
+    f name, summary, first_release_date,
+    genres.name, platforms.abbreviation, 
     screenshots.image_id, artworks.image_id, videos.video_id, 
     age_ratings.rating,
     parent_game.name, parent_game.slug,
+    remakes.name, remakes.slug,
+    remasters.name, remasters.slug,
     collection.name, collection.slug,
     franchises.name, franchises.slug,
+    expanded_games.name, expanded_games.slug,
+    standalone_expansions.name, standalone_expansions.slug,
+    expansions.name, expansions.slug,
     game_modes.name, game_modes.slug,
-    genres.*, themes.*, platforms.*;
+    websites.url, websites.category,
+    alternative_names.comment, alternative_names.name,
+    involved_companies.developer, involved_companies.publisher,
+    involved_companies.company.name,
+    cover.image_id;
     w slug="${slug}";
   `;
   const games: Game[] = await getGames(body);
