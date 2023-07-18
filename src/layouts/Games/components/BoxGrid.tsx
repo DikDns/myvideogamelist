175649127982c48adc7c5bb3a825efd8d12129b8
DikDns <dikdnssocial@gmail.com>
2023-07-18 08:29:34 +0700

    add direction prop in BoxGrid game component

--- src/layouts/Games/components/BoxGrid.tsx
+++ src/layouts/Games/components/BoxGrid.tsx
@@ -2,18 +2,33 @@
 
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
+import Stack from "@mui/material/Stack";
 import { SxProps } from "@mui/material/styles";
 
 export default function BoxGrid({
   label,
   children,
   sx,
+  direction = "column",
 }: {
   label: React.ReactNode;
   children: React.ReactNode;
   sx?: SxProps;
+  direction?: "column" | "row";
 }) {
-  return (
+  return direction === "row" ? (
+    <Stack sx={{ gap: 1, ...sx }}>
+      <Typography variant="body1">
+        <strong
+          style={{ display: "flex", justifyContent: "flex-start", gap: 2 }}
+        >
+          <span>{label}</span>
+          <span>:</span>
+        </strong>
+      </Typography>
+      <Stack>{children}</Stack>
+    </Stack>
+  ) : (
     <Box
       sx={{
         display: "grid",
