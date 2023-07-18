eee7d6b3ee740bc7cb94e84e2f032fa435f16f8e
DikDns <dikdnssocial@gmail.com>
2023-07-18 15:29:52 +0700

    add GameCompany component

--- src/layouts/Games/components/GameInformation.tsx
+++ src/layouts/Games/components/GameInformation.tsx
@@ -2,8 +2,8 @@
 
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-
-import { h4, h5, h6 } from "@/layouts/styles";
+import GameCompany from "./GameCompany";
+import { h4 } from "@/layouts/styles";
 import { Game } from "@/types/Game";
 
 export default function GameInformation({ game }: { game: Game }) {
@@ -15,16 +15,32 @@ export default function GameInformation({ game }: { game: Game }) {
 
       {game.alternative_names ? (
         <Stack>
-          <Typography mb={1} variant="body1">
+          <Typography variant="body1" fontWeight={500}>
             Alternative Titles
           </Typography>
           {game.alternative_names.map((alternative) => (
             <Stack key={alternative.id} direction="row" gap={1}>
-              <Typography variant="body2">{alternative.comment}:</Typography>
-              <Typography variant="body2">{alternative.name}</Typography>
+              <Typography variant="body2" color="#cfcfcf">
+                {alternative.comment}:
+              </Typography>
+              <Typography variant="body2" color="#cfcfcf">
+                {alternative.name}
+              </Typography>
             </Stack>
           ))}
         </Stack>
+      ) : (
+        ""
+      )}
+
+      {game.involved_companies ? (
+        <GameCompany type="developer" data={game.involved_companies} />
+      ) : (
+        ""
+      )}
+
+      {game.involved_companies ? (
+        <GameCompany type="publisher" data={game.involved_companies} />
       ) : (
         ""
       )}
