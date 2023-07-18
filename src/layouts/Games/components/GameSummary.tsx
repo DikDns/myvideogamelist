5611566e5fabff60a683e99d22df71fb289712c8
DikDns <dikdnssocial@gmail.com>
2023-07-18 08:03:38 +0700

    change BoxGrid to external component in GameSummary

--- src/layouts/Games/components/GameSummary.tsx
+++ src/layouts/Games/components/GameSummary.tsx
@@ -1,52 +1,25 @@
 "use client";
 
 import { useState } from "react";
-import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
 import truncStr from "@/utils/truncStr";
 import { Game } from "@/types/Game";
-
-const BoxRowGrid = {
-  display: "grid",
-  gridTemplateColumns: "90px auto",
-  justifyContent: "start",
-  gap: 1,
-};
-
-function BoxRow({
-  label,
-  children,
-}: {
-  label: React.ReactNode;
-  children: React.ReactNode;
-}) {
-  return (
-    <Box sx={BoxRowGrid}>
-      <Typography variant="body1">
-        <strong style={{ display: "flex", justifyContent: "space-between" }}>
-          <span>{label}</span>
-          <span>:</span>
-        </strong>
-      </Typography>
-      <Typography variant="body1">{children}</Typography>
-    </Box>
-  );
-}
+import BoxGrid from "./BoxGrid";
 
 export default function GameSummary({ game }: { game: Game }) {
   const [isReadMoreSumary, setIsReadMoreSummary] = useState(false);
 
   return (
     <Stack gap={2} mb={5} mt={2}>
-      <BoxRow label={`Genre`}>
+      <BoxGrid sx={{ gridTemplateColumns: "90px auto" }} label={`Genre`}>
         {game.genres?.map((genre) => genre.name).join(", ")}
-      </BoxRow>
+      </BoxGrid>
 
-      <BoxRow label={`Platforms`}>
+      <BoxGrid sx={{ gridTemplateColumns: "90px auto" }} label={`Platforms`}>
         {game.platforms?.map((platform) => platform.abbreviation).join(", ")}
-      </BoxRow>
+      </BoxGrid>
 
       <Typography variant="body1">
         {game.summary ? (
