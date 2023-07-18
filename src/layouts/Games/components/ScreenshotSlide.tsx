2aa162bbc28f19eeabc6a863b828a43dd554b30a
DikDns <dikdnssocial@gmail.com>
2023-07-18 09:00:48 +0700

    fix Screenshot type to Image type in ScreenshotSlide component

--- src/layouts/Games/components/ScreenshotSlide.tsx
+++ src/layouts/Games/components/ScreenshotSlide.tsx
@@ -4,14 +4,14 @@ import Box from "@mui/material/Box";
 import CardMedia from "@mui/material/CardMedia";
 import { videoSlide } from "@/layouts/styles";
 import { getImageUrl } from "@/lib/igdb";
-import { Screenshot } from "@/types/Screenshot";
+import { Image } from "@/types/Image";
 import { Game } from "@/types/Game";
 
 export default function ScreenshotSlide({
   screenshot,
   game,
 }: {
-  screenshot: Screenshot;
+  screenshot: Image;
   game: Game;
 }) {
   return (
