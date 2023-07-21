9fcad9d4d064cbd3ab1f00040f49195fa03fe622
DikDns <dikdnssocial@gmail.com>
2023-07-21 16:26:24 +0700

    add settings profile page for testing purpose

---
+++ src/app/settings/profile/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Your Profile",
+};
+
+export default function SettingsProfileLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div>{children}</div>;
+}
