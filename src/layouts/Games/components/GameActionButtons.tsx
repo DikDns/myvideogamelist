a1d5a2922b6087b1bdbbd17f9a77c44e68eebdb4
DikDns <dikdnssocial@gmail.com>
2023-07-21 16:18:16 +0700

    add GameActionButtons session validation

--- src/layouts/Games/components/GameActionButtons.tsx
+++ src/layouts/Games/components/GameActionButtons.tsx
@@ -4,9 +4,19 @@ import { useState } from "react";
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Favorite from "@mui/icons-material/Favorite";
+import { Session } from "next-auth";
+import { signIn } from "next-auth/react";
 
-export default function GameActionButtons() {
+export default function GameActionButtons({
+  session,
+}: {
+  session: Session | null;
+}) {
   const [isFavorited, setIsFavorited] = useState(false);
+
+  const handleDialog = () => {
+    return;
+  };
 
   return (
     <Box
@@ -18,12 +28,18 @@ export default function GameActionButtons() {
         variant="outlined"
         color={isFavorited ? "secondary" : "inherit"}
         sx={{ display: "flex", gap: 1, flexWrap: "wrap" }}
-        onClick={() => setIsFavorited((prevValue) => !prevValue)}
+        onClick={() =>
+          session ? setIsFavorited((prevValue) => !prevValue) : signIn("auth0")
+        }
       >
         <Favorite fontSize="small" />
         {`Favorite${isFavorited ? "d" : ""}`}
       </Button>
-      <Button variant="contained" color="primary">
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={() => (session ? handleDialog() : signIn("auth0"))}
+      >
         {`Add to List`}
       </Button>
     </Box>
