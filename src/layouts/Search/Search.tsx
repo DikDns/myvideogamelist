a8847b3c2a28228837cd6c3a09cec0cd2b302805
DikDns <dikdnssocial@gmail.com>
2023-07-22 17:55:02 +0700

    add mt 10 in Search Layout

--- src/layouts/Search/Search.tsx
+++ src/layouts/Search/Search.tsx
@@ -4,5 +4,9 @@ import { SearchParams } from "@/types/SearchParams";
 import Container from "@mui/material/Container";
 
 export default function Search({ searchParam }: { searchParam: SearchParams }) {
-  return <Container component="main">{searchParam.q}</Container>;
+  return (
+    <Container component="main" sx={{ mt: 10 }}>
+      {searchParam.q}
+    </Container>
+  );
 }
