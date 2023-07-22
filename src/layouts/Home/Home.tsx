de419ae63701ed88378475d921c334d67cfe386a
DikDns <dikdnssocial@gmail.com>
2023-07-22 17:51:45 +0700

    change my Home Layout to 10

--- src/layouts/Home/Home.tsx
+++ src/layouts/Home/Home.tsx
@@ -14,7 +14,7 @@ import { HomeData } from "@/types/HomeData";
 
 export default async function Home({ data }: { data: HomeData }) {
   return (
-    <Container component="main" sx={{ overflow: "hidden", my: 8 }}>
+    <Container component="main" sx={{ overflow: "hidden", my: 10 }}>
       {/* TOP NEW RELEASE */}
       <Box component="section" mb={5}>
         <Typography sx={h1} variant="h1" mb={2}>
