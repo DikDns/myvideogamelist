f497c398b74eb5bf2da21a82fabcc8bae02be579
DikDns <dikdnssocial@gmail.com>
2023-07-22 17:46:21 +0700

    add position fixed in loading index

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -7,6 +7,9 @@ export default function Loading() {
   return (
     <Paper
       sx={{
+        position: "fixed",
+        left: 0,
+        top: 0,
         width: "100vw",
         height: "100vh",
         display: "flex",
