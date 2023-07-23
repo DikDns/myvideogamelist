9c1d649072d78910d530e23c14f2c84cd9ebfe7b
DikDns <dikdnssocial@gmail.com>
2023-07-23 09:07:38 +0700

    add NotFound validation in Search Layout

--- src/layouts/Search/Search.tsx
+++ src/layouts/Search/Search.tsx
@@ -3,6 +3,7 @@
 import { Game } from "@/types/Game";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
 import GameCard from "./components/GameCard";
 
 export default function Search({ games }: { games: Game[] }) {
@@ -10,13 +11,19 @@ export default function Search({ games }: { games: Game[] }) {
 
   return (
     <Container component="main" sx={{ mt: 10 }}>
-      <Grid container gap={4}>
-        {games.map((game) => (
-          <Grid item key={game.id} xs={12}>
-            <GameCard game={game} />
-          </Grid>
-        ))}
-      </Grid>
+      {games.length > 0 ? (
+        <Grid container gap={4}>
+          {games.map((game) => (
+            <Grid item key={game.id} xs={12}>
+              <GameCard game={game} />
+            </Grid>
+          ))}
+        </Grid>
+      ) : (
+        <Typography variant="h6" component="h2" textAlign="center">
+          Not Found
+        </Typography>
+      )}
     </Container>
   );
 }
