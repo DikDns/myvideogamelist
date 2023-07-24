251d5eecd4f37b63528e9bf568fc09c4546832f1
DikDns <dikdnssocial@gmail.com>
2023-07-24 14:24:46 +0700

    add initial Top Upcoming Page

---
+++ src/app/games/top-upcoming/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Top Upcoming | MVGL",
+};
+
+export default function TopUpcomingLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
