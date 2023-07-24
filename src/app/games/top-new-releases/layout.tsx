1e9808c9e54a680056fd0830575c072af7ac588a
DikDns <dikdnssocial@gmail.com>
2023-07-24 14:23:06 +0700

    add initial Top New Releases Page

---
+++ src/app/games/top-new-releases/layout.tsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: "Top New Releases | MVGL",
+};
+
+export default function TopNewReleasesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
