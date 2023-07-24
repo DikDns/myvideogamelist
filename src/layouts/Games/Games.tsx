66b1983c71c2e835973262aeb438fd0f02e43044
DikDns <dikdnssocial@gmail.com>
2023-07-24 14:37:27 +0700

    add Select Input for Bracket Fetch in Games Layout

--- src/layouts/Games/Games.tsx
+++ src/layouts/Games/Games.tsx
@@ -1,32 +1,46 @@
 "use client";
 
-import NextLink from "next/link";
+import * as React from "react";
 import Container from "@mui/material/Container";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import { h2 } from "../styles";
+import Box from "@mui/material/Box";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select, { SelectChangeEvent } from "@mui/material/Select";
+
+export function BracketFetchSelect() {
+  const [bracketFetch, setBracketFetch] = React.useState("top-rated");
+
+  const handleChange = (event: SelectChangeEvent) => {
+    setBracketFetch(event.target.value as string);
+  };
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Bracket</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={bracketFetch}
+          label="Bracket"
+          onChange={handleChange}
+        >
+          <MenuItem value={"top-rated"}>Top Rated</MenuItem>
+          <MenuItem value={"top-new-releases"}>Top New Releases</MenuItem>
+          <MenuItem value={"top-upcoming"}>Top Upcoming</MenuItem>
+          <MenuItem value={"new-releases"}>New Releases</MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
 
 export default function Games() {
   return (
     <Container sx={{ mt: 10 }}>
-      <Typography variant="h2" sx={h2} mb={1}>
-        Category
-      </Typography>
-      <Stack gap={1} direction="row" flexWrap="wrap">
-        <Button LinkComponent={NextLink} href="/games/top-new-releases">
-          Top New Releases
-        </Button>
-        <Button LinkComponent={NextLink} href="/games/top-rated">
-          Top Rated
-        </Button>
-        <Button LinkComponent={NextLink} href="/games/top-upcoming">
-          Top Upcoming
-        </Button>
-        <Button LinkComponent={NextLink} href="/games/new-releases">
-          New Releases
-        </Button>
-      </Stack>
+      <BracketFetchSelect />
     </Container>
   );
 }
