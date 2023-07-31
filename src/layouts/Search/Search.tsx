e8c95d9722d8d6f8b267d3c1fa4d7ffedd8c16f2
DikDns <dikdnssocial@gmail.com>
2023-07-31 10:02:32 +0700

    refactor Search Layput

--- src/layouts/Search/Search.tsx
+++ src/layouts/Search/Search.tsx
@@ -1,23 +1,22 @@
 "use client";
 
-import { Game } from "@/types/Game";
 import InfiniteScroll from "react-infinite-scroll-component";
+import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
 import CircularLoading from "@/components/Loading/CircularLoading";
+import { Game } from "@/types/Game";
 import GameCard from "./components/GameCard";
-import { getGames } from "@/lib/igdb";
-import { SearchParams } from "@/types/SearchParams";
+import getSearchGames from "./server/getSearchGames";
 import { h3 } from "../styles";
-import { useSearchParams } from "next/navigation";
 
 export default function Search() {
   const searchParam = useSearchParams();
   const search = searchParam.get("q");
 
-  const [games, setGames] = useState([]);
+  const [games, setGames] = useState<Game[]>([]);
   const [offset, setOffset] = useState(0);
   const [hasMore, setHasMore] = useState(true);
 
@@ -27,21 +26,14 @@ export default function Search() {
     setOffset(0);
   }, [search]);
 
+  useEffect(() => {
+    if (games.length > 0) return;
+    fetchMore();
+  });
+
   const fetchMore = async () => {
-    let body = `
-    f name, slug, cover.image_id, aggregated_rating, genres.name;
-    w aggregated_rating != n & aggregated_rating_count > 7;
-    s aggregated_rating desc;
-    l 10;
-    o ${offset};
-  `;
-
-    if (search && search.length > 1) {
-      body = `search "${search}"; f name, aggregated_rating, category, slug, cover.image_id, genres.name, cover.image_id; l 10; o ${offset};`;
-    }
-
-    const nextGames: Game[] = await getGames(body);
-    nextGames.sort((a, b) => (a.category || 0) - (b.category || 0));
+    const fetchLimit = 10;
+    const nextGames = await getSearchGames(search, fetchLimit, offset);
 
     if (nextGames.length <= 0) return setHasMore(false);
 
@@ -52,29 +44,32 @@ export default function Search() {
   return (
     <Container component="main" sx={{ my: 11 }}>
       <Typography variant="h3" sx={h3} mb={2}>
-        {`Search result: ${searchParams.q}`}
+        {`Search result: ${search}`}
       </Typography>
 
-      {initGames.length > 0 ? (
-        <InfiniteScroll
-          dataLength={games.length} //This is important field to render the next data
-          next={fetchMore}
-          hasMore={hasMore}
-          loader={<CircularLoading />}
-        >
-          <Grid container gap={3}>
-            {games.map((game, i) => (
-              <Grid item key={i} xs={12}>
-                <GameCard game={game} />
-              </Grid>
-            ))}
-          </Grid>
-        </InfiniteScroll>
-      ) : (
-        <Typography variant="h6" component="h2" textAlign="center">
-          Not Found
-        </Typography>
-      )}
+      <InfiniteScroll
+        dataLength={games.length} //This is important field to render the next data
+        next={fetchMore}
+        hasMore={hasMore}
+        loader={<CircularLoading />}
+        endMessage={
+          games.length === 0 ? (
+            <Typography variant="h6" component="h2" textAlign="center">
+              Not Found
+            </Typography>
+          ) : (
+            ""
+          )
+        }
+      >
+        <Grid container gap={3}>
+          {games.map((game, i) => (
+            <Grid item key={i} xs={12}>
+              <GameCard game={game} />
+            </Grid>
+          ))}
+        </Grid>
+      </InfiniteScroll>
     </Container>
   );
 }
