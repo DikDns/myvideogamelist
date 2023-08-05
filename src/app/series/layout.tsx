b23f16c82f5c89ce953a13655a479fb0bf568adc
DikDns <dikdnssocial@gmail.com>
2023-08-05 09:56:54 +0700

    add initial series page file

---
+++ src/app/series/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Games",
+  description:
+    "Welcome to MVGL Series Section! The place where you can search your favorite Series!",
+};
+
+export default function SeriesLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
