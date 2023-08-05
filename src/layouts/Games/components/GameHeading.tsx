680b8f9cff30097bf9dbd30efc7e19a82e636197
DikDns <dikdnssocial@gmail.com>
2023-08-05 09:34:59 +0700

    fix GameHeading component bg gradient to Box

--- src/layouts/Games/components/GameHeading.tsx
+++ src/layouts/Games/components/GameHeading.tsx
@@ -32,59 +32,68 @@ export default function GameHeading({ game }: { game: Game }) {
         )});`,
       }}
     >
-      <Container>
-        <Grid
-          alignItems="center"
-          flexWrap="nowrap"
-          container
-          sx={{
-            py: {
-              xs: 2,
-              md: 1,
-            },
-            gap: 2,
-            background:
-              "linear-gradient(to bottom, #12121200 0%, #121212b2 50%, #121212 100%)",
-            backdropFilter: "blur(4px)",
-          }}
-        >
-          <Grid item sx={{ borderRadius: "2px", overflow: "hidden" }} xs="auto">
-            <Image
-              src={getImageUrl(game.cover?.image_id || "", "cover_big", "2x")}
-              alt={`${game.name} Cover`}
-              width={matches ? 180 : 90}
-              height={matches ? 256 : 128}
-            />
-          </Grid>
-          <Grid container item direction="column" xs={10} gap={1}>
-            <Grid item>
-              <Typography sx={h2} variant="h2">
-                {game.name}
-              </Typography>
+      <Box
+        sx={{
+          background:
+            "linear-gradient(to bottom, #12121200 0%, #121212b2 50%, #121212 100%)",
+          backdropFilter: "blur(4px)",
+        }}
+      >
+        <Container>
+          <Grid
+            alignItems="center"
+            flexWrap="nowrap"
+            container
+            sx={{
+              py: {
+                xs: 2,
+                md: 1,
+              },
+              gap: 2,
+            }}
+          >
+            <Grid
+              item
+              sx={{ borderRadius: "2px", overflow: "hidden" }}
+              xs="auto"
+            >
+              <Image
+                src={getImageUrl(game.cover?.image_id || "", "cover_big", "2x")}
+                alt={`${game.name} Cover`}
+                width={matches ? 180 : 90}
+                height={matches ? 256 : 128}
+              />
             </Grid>
-            <Grid item>
-              <Typography sx={h3} variant="h3">
-                {game.first_release_date
-                  ? `${formatUnix(game.first_release_date || 0)}`
-                  : ""}
-              </Typography>
-            </Grid>
-            <Grid item>
-              <Typography
-                className="name"
-                variant="caption"
-                sx={{ display: "flex", alignItems: "center", gap: "2px" }}
-              >
-                <Star fontSize="inherit" color="secondary" />
-                {game.aggregated_rating
-                  ? `${Math.round(game.aggregated_rating / 10)} from
+            <Grid container item direction="column" xs={10} gap={1}>
+              <Grid item>
+                <Typography sx={h2} variant="h2">
+                  {game.name}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography sx={h3} variant="h3">
+                  {game.first_release_date
+                    ? `${formatUnix(game.first_release_date || 0)}`
+                    : ""}
+                </Typography>
+              </Grid>
+              <Grid item>
+                <Typography
+                  className="name"
+                  variant="caption"
+                  sx={{ display: "flex", alignItems: "center", gap: "2px" }}
+                >
+                  <Star fontSize="inherit" color="secondary" />
+                  {game.aggregated_rating
+                    ? `${Math.round(game.aggregated_rating / 10)} from
                 ${game.aggregated_rating_count} critic reviews`
-                  : "Not Rated"}{" "}
-              </Typography>
+                    : "Not Rated"}{" "}
+                </Typography>
+              </Grid>
             </Grid>
           </Grid>
-        </Grid>
-      </Container>
+        </Container>
+      </Box>
     </Box>
   );
 }
