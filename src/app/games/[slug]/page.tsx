f2122e04a920f8ac4b6506f254fa779a3cea0d78
DikDns <dikdnssocial@gmail.com>
2023-08-05 10:26:11 +0700

    remove generateMetada on game detail page

--- src/app/games/[slug]/page.tsx
+++ src/app/games/[slug]/page.tsx
@@ -1,18 +1,8 @@
-import { Metadata, ResolvingMetadata } from "next";
 import { PageProps } from "@/types/PageProps";
-import getGamePageData, {
-  getGamePageMetadata,
-} from "@/layouts/Games/Server/getGamePageData";
+import getGamePageData from "@/layouts/Games/Server/getGamePageData";
 import Game from "@/layouts/Games/Game";
 
 type GamePageProps = PageProps<{ slug: string }>;
-
-export async function generateMetadata(
-  { params: { slug }, searchParams }: GamePageProps,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  return await getGamePageMetadata(slug);
-}
 
 export default async function GamePage({ params: { slug } }: GamePageProps) {
   const { session, games, gameListUser } = await getGamePageData(slug);
