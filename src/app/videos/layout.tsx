435f3f47b692531b4f4b1fe14f6ef5efe204d2eb
DikDns <dikdnssocial@gmail.com>
2023-08-10 08:09:41 +0700

    feat: add initial videos page

---
+++ src/app/videos/layout.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: "Videos",
+  description:
+    "Welcome to MVGL Videos Section! The place where you can find your favorite games videos!",
+};
+
+export default function VideosLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return children;
+}
