95ca653114562c386496ea794269e4645287e529
DikDns <dikdnssocial@gmail.com>
2023-08-14 08:42:35 +0700

    feat: Bio controlled in client

--- src/layouts/Profile/ProfileControl.tsx
+++ src/layouts/Profile/ProfileControl.tsx
@@ -5,13 +5,9 @@ import Image from "next/image";
 import NextLink from "next/link";
 import Container from "@mui/material/Container";
 import Stack from "@mui/material/Stack";
-import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Link from "@mui/material/Link";
-import IconButton from "@mui/material/IconButton";
-import Button from "@mui/material/Button";
-import TextField from "@mui/material/TextField";
-import EditIcon from "@mui/icons-material/Edit";
+import Box from "@mui/material/Box";
 import BioControlled from "./components/BioControlled";
 import { h2 } from "../styles";
 
@@ -25,7 +21,7 @@ export default function ProfileControl({ user }: { user: User }) {
   return (
     <Container>
       <Stack gap={2}>
-        <Stack direction="row" gap={2}>
+        <Box sx={{ display: "grid", gridTemplateColumns: "auto 1fr", gap: 2 }}>
           <Image
             src={user.image || ""}
             alt={`${user.username} picture`}
@@ -37,23 +33,10 @@ export default function ProfileControl({ user }: { user: User }) {
             <Typography variant="h2" sx={h2}>
               {`${user.username?.toUpperCase()}`}
             </Typography>
-            <Typography variant="body2">
-              {`${user.bio || "A gamer who hasn't set their bio yet."}`}
-              {` Click the pen icon on the bottom to edit your bio.`}
-            </Typography>
-
-            <Button
-              aria-label="edit bio"
-              variant="text"
-              color="info"
-              // onClick={}
-            >
-              <EditIcon sx={{ mr: 1 }} /> Edit Bio
-            </Button>
 
             <BioControlled user={user} />
           </Stack>
-        </Stack>
+        </Box>
 
         <Stack direction="row" gap={2} justifyContent="space-evenly">
           <Link
--- src/layouts/Profile/components/BioControlled.tsx
+++ src/layouts/Profile/components/BioControlled.tsx
@@ -1,17 +1,70 @@
 "use client";
 
-import { useState } from "react";
+import {
+  useState,
+  Dispatch,
+  SetStateAction,
+  ChangeEvent,
+  MouseEvent,
+} from "react";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import FormHelperText from "@mui/material/FormHelperText";
+import Typography from "@mui/material/Typography";
 import SaveIcon from "@mui/icons-material/Save";
+import EditIcon from "@mui/icons-material/Edit";
 import { User } from "../User";
 
-export default function BioControlled({ user }: { user: User }) {
-  const bioMaxLength = 190;
-  const [bio, setBio] = useState(user.bio || "");
+const bioMaxLength = 190;
+const defaultBio = "A gamer who hasn't set their bio yet.";
 
-  const handleBioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+export default function BioControlled({ user }: { user: User }) {
+  const [bio, setBio] = useState(user.bio || defaultBio);
+  const [onEdit, setOnEdit] = useState(false);
+
+  return (
+    <Box>
+      {!onEdit && <BioDisplay bio={bio} setOnEdit={setOnEdit} />}
+      {onEdit && <BioInput bio={bio} setBio={setBio} setOnEdit={setOnEdit} />}
+    </Box>
+  );
+}
+
+function BioDisplay({
+  bio,
+  setOnEdit,
+}: {
+  bio: string;
+  setOnEdit: Dispatch<SetStateAction<boolean>>;
+}) {
+  return (
+    <Stack gap={1}>
+      <Typography variant="body2">{bio}</Typography>
+
+      <Button
+        aria-label="edit bio"
+        variant="text"
+        color="info"
+        onClick={() => setOnEdit(true)}
+      >
+        <EditIcon sx={{ mr: 1 }} /> Edit Bio
+      </Button>
+    </Stack>
+  );
+}
+
+function BioInput({
+  bio,
+  setBio,
+  setOnEdit,
+}: {
+  bio: string;
+  setBio: Dispatch<SetStateAction<string>>;
+  setOnEdit: Dispatch<SetStateAction<boolean>>;
+}) {
+  const handleBioChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.value.length > bioMaxLength) return;
     if (bio.length > bioMaxLength) {
       return setBio(event.target.value.substring(0, bioMaxLength));
@@ -19,26 +72,33 @@ export default function BioControlled({ user }: { user: User }) {
     setBio(event.target.value);
   };
 
+  const handleBioSave = (event: MouseEvent<HTMLButtonElement>) => {
+    setOnEdit(false);
+  };
+
   return (
-    <>
-      <TextField
-        id={`${user.username}-bio`}
-        label={`Bio`}
-        multiline
-        value={bio}
-        onChange={handleBioChange}
-        aria-describedby="max-bio-char-length"
-      />
-      <FormHelperText id="max-bio-char-length">{`${bio.length}/${bioMaxLength}`}</FormHelperText>
+    <Stack mt={1} gap={1}>
+      <Box>
+        <TextField
+          fullWidth
+          id={`user-bio`}
+          label={`Bio`}
+          multiline
+          value={bio}
+          onChange={handleBioChange}
+          aria-describedby="max-bio-char-length"
+        />
+        <FormHelperText id="max-bio-char-length">{`${bio.length}/${bioMaxLength}`}</FormHelperText>
+      </Box>
       <Button
         aria-label="save bio"
         variant="text"
         color="secondary"
-        // onClick={}
+        onClick={handleBioSave}
         disabled={bio.length > bioMaxLength}
       >
         <SaveIcon sx={{ mr: 1 }} /> Save Bio
       </Button>
-    </>
+    </Stack>
   );
 }
