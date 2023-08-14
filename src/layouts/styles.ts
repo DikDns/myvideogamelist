041ad5e9c1ce3e966dc27f69714e653abe303d16
DikDns <dikdnssocial@gmail.com>
2023-08-14 13:52:41 +0700

    fix: slideMargin mx to xs: 1

--- src/layouts/styles.ts
+++ src/layouts/styles.ts
@@ -59,7 +59,7 @@ export const h6: SxProps = {
   fontWeight: "300",
 };
 
-export const slideMargin: SxProps = { mx: { sm: 1, md: 2 } };
+export const slideMargin: SxProps = { mx: { xs: 1, md: 2 } };
 
 export const videoSlide: SxProps = {
   width: "256px",
