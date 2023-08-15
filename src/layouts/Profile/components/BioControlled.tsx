e629929e38a61faec3ceef89fb35ae54ec4306e0
DikDns <dikdnssocial@gmail.com>
2023-08-15 08:30:17 +0700

    feat: steamUsername client interaction profile

--- src/layouts/Profile/components/BioControlled.tsx
+++ src/layouts/Profile/components/BioControlled.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import NextLink from "next/link";
 import {
   useState,
   Dispatch,
@@ -14,6 +15,7 @@ import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
 import FormHelperText from "@mui/material/FormHelperText";
 import Typography from "@mui/material/Typography";
+import Link from "@mui/material/Link";
 import SaveIcon from "@mui/icons-material/Save";
 import EditIcon from "@mui/icons-material/Edit";
 import { User } from "../User";
@@ -24,16 +26,48 @@ const defaultBio = "A gamer who hasn't set their bio yet.";
 
 export default function BioControlled({ user }: { user: User }) {
   const [bio, setBio] = useState(user.bio || defaultBio);
+  const [steamUsername, setSteamUsername] = useState(
+    user.gameSocialNetwork?.steamUsername || ""
+  );
   const [onEdit, setOnEdit] = useState(false);
 
   return (
     <Box>
-      {!onEdit && <BioDisplay bio={bio} setOnEdit={setOnEdit} />}
+      {!onEdit && (
+        <Stack gap={1}>
+          <Typography variant="body2">{bio}</Typography>
+
+          <Typography variant="caption">
+            {steamUsername ? (
+              <Link
+                component={NextLink}
+                href={`https://steamcommunity.com/id/${steamUsername}`}
+                color="#fff"
+              >
+                {`steamcommunity.com/id/${steamUsername}`}
+              </Link>
+            ) : (
+              "steamcommunity.com/id/"
+            )}
+          </Typography>
+
+          <Button
+            aria-label="edit bio"
+            variant="text"
+            color="info"
+            onClick={() => setOnEdit(true)}
+          >
+            <EditIcon sx={{ mr: 1 }} /> Edit
+          </Button>
+        </Stack>
+      )}
       {onEdit && (
         <BioInput
           userId={user.id}
           bio={bio}
           setBio={setBio}
+          steamUsername={steamUsername}
+          setSteamUsername={setSteamUsername}
           setOnEdit={setOnEdit}
         />
       )}
@@ -41,41 +75,30 @@ export default function BioControlled({ user }: { user: User }) {
   );
 }
 
-function BioDisplay({
-  bio,
-  setOnEdit,
-}: {
-  bio: string;
-  setOnEdit: Dispatch<SetStateAction<boolean>>;
-}) {
-  return (
-    <Stack gap={1}>
-      <Typography variant="body2">{bio}</Typography>
-
-      <Button
-        aria-label="edit bio"
-        variant="text"
-        color="info"
-        onClick={() => setOnEdit(true)}
-      >
-        <EditIcon sx={{ mr: 1 }} /> Edit Bio
-      </Button>
-    </Stack>
-  );
-}
-
 function BioInput({
   userId,
   bio,
   setBio,
+  steamUsername,
+  setSteamUsername,
   setOnEdit,
 }: {
   userId: string;
   bio: string;
   setBio: Dispatch<SetStateAction<string>>;
+  steamUsername: string;
+  setSteamUsername: Dispatch<SetStateAction<string>>;
   setOnEdit: Dispatch<SetStateAction<boolean>>;
 }) {
   const [isPending, startTransition] = useTransition();
+
+  const handleSteamUsernameChange = (event: ChangeEvent<HTMLInputElement>) => {
+    if (event.target.value.length > bioMaxLength) return;
+    if (bio.length > bioMaxLength) {
+      return setSteamUsername(event.target.value.substring(0, bioMaxLength));
+    }
+    setSteamUsername(event.target.value);
+  };
 
   const handleBioChange = (event: ChangeEvent<HTMLInputElement>) => {
     if (event.target.value.length > bioMaxLength) return;
@@ -104,6 +127,14 @@ function BioInput({
         />
         <FormHelperText id="max-bio-char-length">{`${bio.length}/${bioMaxLength}`}</FormHelperText>
       </Box>
+      <TextField
+        fullWidth
+        id={`user-steam`}
+        label={`Steam Username`}
+        multiline
+        value={steamUsername}
+        onChange={handleSteamUsernameChange}
+      />
       <Button
         aria-label="save bio"
         variant="text"
