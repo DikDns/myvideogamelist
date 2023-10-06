2542edae12bdb419c28b3a3d9186f827e36f4737
DikDns <dikdnssocial@gmail.com>
2023-10-06 13:39:55 +0700

    #10 [games] feat: refactor GameAgeRatings

---
+++ src/features/games/components/GameAgeRatings.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import Image from "next/image";
+import igdbAgeRating from "../lib/igdbAgeRating";
+import AgeRating from "../types/AgeRating";
+
+function AgeRatingImage({ url, title }: { url: string; title: string }) {
+  return <Image src={url} alt={title} width={48} height={48} />;
+}
+
+export default function GameAgeRatings({
+  ageRatings,
+}: {
+  ageRatings: AgeRating[];
+}) {
+  return ageRatings.map((ageRating) => {
+    if (!ageRating.rating) return;
+    const { url, title } = igdbAgeRating(ageRating.rating);
+    return <AgeRatingImage key={ageRating.id} url={url} title={title} />;
+  });
+}
