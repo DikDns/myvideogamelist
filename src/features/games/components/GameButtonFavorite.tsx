da938d1cca7c1a19ed6bc05bd5aae5b9aeeb1851
DikDns <dikdnssocial@gmail.com>
2023-10-06 12:48:59 +0700

    #10 [games] fix: rename to GameButtonFavorite

--- src/features/games/components/GameButtonFavorite.tsx
+++ src/features/games/components/GameButtonFavorite.tsx
@@ -1,20 +1,18 @@
 "use client";
 
-import { useAuth } from "@clerk/nextjs";
 import { experimental_useOptimistic as useOptimistic } from "react";
-import Button from "@mui/material/Button";
-import Favorite from "@mui/icons-material/Favorite";
-import { updateIsFavorited } from "../Server/Actions";
-import { Game } from "@/types/Game";
+import MUIButton from "@mui/material/Button";
+import IMFavorite from "@mui/icons-material/Favorite";
+import { updateIsFavorited } from "../lib/nextServerFavoriteButton";
+import Game from "../types/Game";
 
-export default function GameFavoriteButton({
+export default function GameButtonFavorite({
   isFavorited,
   game,
 }: {
   isFavorited: boolean | null;
   game: Game;
 }) {
-  const { userId } = useAuth();
   const [favoriteOptimistic, addFavoriteOptimistic] = useOptimistic(
     {
       isFavorited: isFavorited,
@@ -29,15 +27,11 @@ export default function GameFavoriteButton({
 
   const handleFavorite = async () => {
     addFavoriteOptimistic(!favoriteOptimistic.isFavorited);
-    await updateIsFavorited(
-      userId || "",
-      game.id,
-      !favoriteOptimistic.isFavorited
-    );
+    await updateIsFavorited(game.id, !favoriteOptimistic.isFavorited);
   };
 
   return (
-    <Button
+    <MUIButton
       variant="outlined"
       color={favoriteOptimistic.isFavorited ? "secondary" : "inherit"}
       sx={{
@@ -48,8 +42,8 @@ export default function GameFavoriteButton({
       }}
       onClick={() => handleFavorite()}
     >
-      <Favorite fontSize="small" />
+      <IMFavorite fontSize="small" />
       {`Favorite${favoriteOptimistic.isFavorited ? "d" : ""}`}
-    </Button>
+    </MUIButton>
   );
 }
