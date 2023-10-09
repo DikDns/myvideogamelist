954eb8fde16f672d2f734719d7329d8d6a4e2963
DikDns <dikdnssocial@gmail.com>
2023-10-09 09:44:01 +0700

    #10 [games] refactor GameMainLayout

--- src/features/games/components/GameMainLayout/GameMainLayout.tsx
+++ src/features/games/components/GameMainLayout/GameMainLayout.tsx
@@ -2,9 +2,9 @@
 
 import MUIBox from "@mui/material/Box";
 import BasicBreadcrumbs from "@/components/Navigation/BasicBreadcrumbs";
-import GameButtons from "./GameButtons";
+import GameButtons from "./GameButtons/GameButtons";
+import GameMedia from "./GameMedia/GameMedia";
 import GameSummary from "./GameSummary";
-import GameMedia from "./GameMedia";
 import GameRelatedContent from "./GameRelatedContent";
 import GameResources from "./GameResources";
 
