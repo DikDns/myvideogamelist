f12daad4cc3dccc18956fbd97152c1101a908e5d
DikDns <dikdnssocial@gmail.com>
2023-10-09 09:49:46 +0700

    #10 [games] refactor: GameLayout

--- src/features/games/components/GameLayout.tsx
+++ src/features/games/components/GameLayout.tsx
@@ -2,27 +2,38 @@
 
 import MUIContainer from "@mui/material/Container";
 import MUIBox from "@mui/material/Box";
-import BasicBreadcrumbs from "@/components/Navigation/BasicBreadcrumbs";
+import GameMainLayout from "./GameMainLayout/GameMainLayout";
 import GameHeading from "./GameHeading";
-import GameSummary from "./GameSummary";
-import GameMedia from "./GameMedia";
-import GameRelatedContent from "./GameRelatedContent";
-import GameResources from "./GameResources";
 import GameInformation from "./GameInformation";
 import GameRecommendation from "./GameRecommendation";
-import GameButtons from "./GameButtons";
 import { ListStatus } from "@prisma/client";
 import { createContext } from "react";
 import Game from "../types/Game";
 
+/**
+ * Represents the user's game list.
+ */
 type GameListUser = {
   isFavorited: boolean | null;
   status: ListStatus | null;
   score: number | null;
 };
 
+/**
+ * Context for the current game being displayed.
+ */
 export const GameContext = createContext<Game | null>(null);
 
+/**
+ * Context for the user's game list.
+ */
+export const GameListUserContext = createContext<GameListUser | null>(null);
+
+/**
+ * Renders the layout for a single game.
+ * @param game - The game to display.
+ * @param userGameList - The user's game list.
+ */
 export default function Game({
   game,
   userGameList,
@@ -32,32 +43,35 @@ export default function Game({
 }) {
   return (
     <GameContext.Provider value={game}>
-      {/* Heading: Game Cover and Name */}
-      {/*// TODO add aggregated rating display */}
-      <GameHeading game={game} />
+      <GameListUserContext.Provider value={userGameList}>
+        <GameHeading />
 
-      {/* Game Information: Genres, Platforms, Summary */}
-      <MUIContainer sx={{ overflow: "hidden" }}>
-        <MUIBox
-          sx={{
-            display: { md: "grid" },
-            gridTemplateColumns: { md: "75% 25%" },
-          }}
-        >
-          <MUIBox component="main">
-            <BasicBreadcrumbs />
-            <GameButtons game={game} gameListUser={userGameList} />
-            <GameSummary game={game} />
-            <GameMedia game={game} />
-            <GameRelatedContent game={game} />
-            <GameResources game={game} />
+        <MUIContainer sx={{ overflow: "hidden" }}>
+          <MUIBox
+            sx={{
+              display: { md: "grid" },
+              gridTemplateColumns: { md: "75% 25%" },
+            }}
+          >
+            <GameMainLayout />
+
+            <GameAsideLayout />
           </MUIBox>
-          <MUIBox component="aside">
-            <GameInformation game={game} />
-          </MUIBox>
-        </MUIBox>
-        <GameRecommendation game={game} />
-      </MUIContainer>
+
+          <GameRecommendation />
+        </MUIContainer>
+      </GameListUserContext.Provider>
     </GameContext.Provider>
+  );
+}
+
+/**
+ * Renders the layout for the game information aside.
+ */
+function GameAsideLayout() {
+  return (
+    <MUIBox component="aside">
+      <GameInformation />
+    </MUIBox>
   );
 }
