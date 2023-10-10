0c1620e07e40cdbfbb1389fed732f71b8db6b7d1
DikDns <dikdnssocial@gmail.com>
2023-10-10 09:57:43 +0700

    #10 [home] refactor: VideosNew

---
+++ src/features/home/components/VideosNew.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import NextLink from "next/link";
+import { useContext } from "react";
+import MUIBox from "@mui/material/Box";
+import MUILink from "@mui/material/Link";
+import MUITypography from "@mui/material/Typography";
+import { SxProps } from "@mui/material/styles";
+import SlideVideo from "./Carousel/SlideVideo";
+import CarouselMedia from "./Carousel/CarouselMedia";
+import { HomeDataContext } from "./HomeLayout";
+
+const h3: SxProps = {
+  fontSize: {
+    xs: "1.25rem",
+    sm: "2rem",
+    md: "2.5rem",
+  },
+  fontWeight: "500",
+  letterSpacing: "0.5px",
+};
+
+export default function VideosNew() {
+  const { newTrailers } = useContext(HomeDataContext);
+
+  return (
+    <MUIBox component="section" mb={5}>
+      <MUITypography sx={h3} variant="h3" mb={2}>
+        {"New Trailers"}
+      </MUITypography>
+
+      <CarouselMedia
+        slides={newTrailers.map((trailer) => (
+          <SlideVideo key={trailer.id} video={trailer} />
+        ))}
+      />
+
+      <MUIBox mt={1}>
+        <MUILink component={NextLink} href="/videos">
+          See More
+        </MUILink>
+      </MUIBox>
+    </MUIBox>
+  );
+}
