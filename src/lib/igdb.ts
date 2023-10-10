1dc22146cc76ded711752a1a75173f103a5ef9bc
DikDns <dikdnssocial@gmail.com>
2023-10-10 09:59:29 +0700

    fix: remove home igdb api

--- src/lib/igdb.ts
+++ src/lib/igdb.ts
@@ -17,20 +17,11 @@ type ImageType =
   | "720p"
   | "1080p";
 
-function currentDate() {
-  const currentDate = new Date();
-  const unixTimestamp = Math.floor(currentDate.getTime() / 1000);
-  return unixTimestamp;
-}
-
-function lastMonthDate(date: number = -30) {
-  const lastMonthDate = new Date();
-  lastMonthDate.setDate(date);
-  const unixTimestamp = Math.floor(lastMonthDate.getTime() / 1000);
-  return unixTimestamp;
-}
-
-const defaultField = `name, cover.image_id, slug, first_release_date, aggregated_rating, category, genres.name`;
+export const defaultField = `
+    name, cover.image_id, 
+    slug, first_release_date, aggregated_rating, category, 
+    genres.name
+  `;
 
 export async function fetchIGDBToken() {
   const baseUrl = process.env.TWITCH_TOKEN_URL ?? "";
@@ -45,7 +36,7 @@ export async function fetchIGDBToken() {
   if (!res.ok) return Promise.reject(res);
 
   const { access_token, expires_in } = await res.json();
-  let expiredAt = new Date(res.headers.get("date") ?? "");
+  const expiredAt = new Date(res.headers.get("date") ?? "");
   expiredAt.setSeconds(expires_in);
 
   return {
@@ -79,91 +70,6 @@ export async function getGameVideos(body: string) {
 
 export async function getCompanies(body: string) {
   return await get("companies", body);
-}
-
-export async function getTopNewReleaseGames(
-  limit: number = 20,
-  offset: number = 0
-) {
-  const body = `
-    f ${defaultField};
-    w first_release_date > ${lastMonthDate(-60)} & 
-    first_release_date < ${currentDate()} & 
-    hypes != n & cover != n & aggregated_rating != n;
-    s hypes desc;
-    l ${limit};
-    o ${offset};
-  `;
-  return await getGames(body);
-}
-
-export async function getTopFranchises() {
-  const body = `
-    f name, games.cover.image_id, slug;
-    w games.total_rating_count > 1000;
-    s games desc;
-    l 20;
-  `;
-  return await getFranchises(body);
-}
-
-export async function getTopSeries() {
-  const body = `
-    f name, games.cover.image_id, slug;
-    w games.total_rating_count > 1000;
-    s games desc;
-    l 20;
-  `;
-  return await getSeries(body);
-}
-
-export async function getNewTrailers() {
-  const body = `
-    f name, video_id, game.name;
-    w name~*"trailer"* & game.age_ratings != n;
-    s id desc;
-    l 10;
-  `;
-  return await getGameVideos(body);
-}
-
-export async function getPopularUpcomingGames(
-  limit: number = 5,
-  offset: number = 0
-) {
-  const body = `
-    f ${defaultField};
-    w first_release_date > ${currentDate()} & hypes != n & cover != n;
-    s hypes desc;
-    l ${limit};
-    o ${offset};
-  `;
-  return await getGames(body);
-}
-
-export async function getNewReleaseGames(
-  limit: number = 5,
-  offset: number = 0
-) {
-  const body = `
-    f ${defaultField};
-    w first_release_date < ${currentDate()} & cover != n & genres != n;
-    s first_release_date desc;
-    l ${limit};
-    o ${offset};
-  `;
-  return await getGames(body);
-}
-
-export async function getTopRatedGames(limit: number = 5, offset: number = 0) {
-  const body = `
-    f ${defaultField};
-    w aggregated_rating != n & aggregated_rating_count > 7;
-    s aggregated_rating desc;
-    l ${limit};
-    o ${offset};
-  `;
-  return await getGames(body);
 }
 
 export async function searchGames(
