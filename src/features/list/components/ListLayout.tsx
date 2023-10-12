66ce8311daeeaa08d2182b4e0c411a4481579280
DikDns <dikdnssocial@gmail.com>
2023-10-12 17:18:11 +0700

    #10 [list] refactor: ListLayout

---
+++ src/features/list/components/ListLayout.tsx
@@ -0,0 +1,29 @@
+"use server";
+
+import { currentUser as clerkCurrentUser } from "@clerk/nextjs";
+import { currentUserList } from "@/features/list/lib/nextServerListLayout";
+import List from "./List";
+import ListControl from "./ListControl";
+import { PageProps } from "@/types/PageProps";
+
+type ListLayoutProps = PageProps<{ username: string }>;
+
+export const revalidate = 0;
+
+export default async function ListLayout({
+  params: { username },
+}: ListLayoutProps) {
+  const userPromise = currentUserList(username);
+  const currentUserPromise = clerkCurrentUser();
+
+  const [user, currentUser] = await Promise.all([
+    userPromise,
+    currentUserPromise,
+  ]);
+
+  if (currentUser?.username !== username) {
+    return <List user={user} />;
+  }
+
+  return <ListControl user={user} />;
+}
