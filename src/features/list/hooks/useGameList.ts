52d2e07dc8027ea3a85f82c47462afd69a96d31a
DikDns <dikdnssocial@gmail.com>
2023-10-12 17:45:20 +0700

    #10 [list] refactor: useGameList Hooks

---
+++ src/features/list/hooks/useGameList.ts
@@ -0,0 +1,73 @@
+"use client";
+
+import { useState, useEffect } from "react";
+import { getGames } from "@/lib/igdb";
+import GameList, { User } from "../types/GameList";
+import { Game } from "@/types/Game";
+import { GameList as prismaGameList } from "@prisma/client";
+
+export default function useGameList(user: User | null) {
+  const [gameList, setGameList] = useState<GameList[]>([]);
+  const [hasMore, setHasMore] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (user?.gameList.length === 0) return;
+    return () => {
+      fetchMore();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (gameList.length === user?.gameList.length) {
+      setIsLoading(false);
+      setHasMore(false);
+      return;
+    }
+    setHasMore(true);
+  }, [gameList]);
+
+  const fetchMore = async () => {
+    setIsLoading(true);
+    const fetchLimit = 20;
+    const currentGameList: prismaGameList[] = [];
+
+    for (let i = gameList.length; i < gameList.length + fetchLimit; i++) {
+      if (user?.gameList[i]) {
+        currentGameList.push(user?.gameList[i]);
+      }
+    }
+
+    const body = `
+      f name, slug, cover.image_id;
+      l ${fetchLimit};
+      w id=(${currentGameList.map((list) => list.gameId).join(",")});
+    `;
+
+    const nextGame: Game[] = await getGames(body);
+
+    const nextGameList: GameList[] = [];
+
+    for (const currentGameListElement of currentGameList) {
+      const currentGame = nextGame.find(
+        (item) => item.id === currentGameListElement.gameId
+      );
+
+      if (currentGame) {
+        nextGameList.push({
+          userId: user!.id,
+          gameId: currentGame.id,
+          game: currentGame,
+          isFavorited: currentGameListElement.isFavorited,
+          score: currentGameListElement.score,
+          status: currentGameListElement.status,
+        });
+      }
+    }
+
+    setGameList((prevGameList) => [...prevGameList, ...nextGameList]);
+    setIsLoading(false);
+  };
+
+  return { gameList, isLoading, hasMore, fetchMore };
+}
