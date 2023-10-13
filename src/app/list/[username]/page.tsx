32479f856b1098ad5325e32e63c9da61cca790e0
DikDns <dikdnssocial@gmail.com>
2023-10-13 18:46:23 +0700

    #10 [list] feat: ListLayout Page

--- src/app/list/[username]/page.tsx
+++ src/app/list/[username]/page.tsx
@@ -1,35 +1,5 @@
-import { currentUser as clerkCurrentUser } from "@clerk/nextjs";
-import getList from "@/layouts/List/getList";
-import List from "@/layouts/List/List";
-import ListControl from "@/layouts/List/ListControl";
-import { PageProps } from "@/types/PageProps";
-
-type ListPageProps = PageProps<{ username: string }>;
+import ListLayout from "@/features/list/components/ListLayout";
 
 export const revalidate = 0;
 
-export default async function ListUsernamePage({
-  params: { username },
-}: ListPageProps) {
-  const userPromise = getList(username);
-  const currentUserPromise = clerkCurrentUser();
-
-  const [user, currentUser] = await Promise.all([
-    userPromise,
-    currentUserPromise,
-  ]);
-
-  if (currentUser?.username !== username) {
-    return (
-      <div>
-        <List user={user} />
-      </div>
-    );
-  }
-
-  return (
-    <div>
-      <ListControl user={user} />
-    </div>
-  );
-}
+export default ListLayout;
