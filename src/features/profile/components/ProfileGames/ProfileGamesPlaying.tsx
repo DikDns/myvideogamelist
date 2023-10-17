65f598cd31005cb07a2778020f5fe4c5967223e1
DikDns <dikdnssocial@gmail.com>
2023-10-17 08:17:06 +0700

    #10 [profile] refactor: ProfileGamesPlaying

---
+++ src/features/profile/components/ProfileGames/ProfileGamesPlaying.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import MUIStack from "@mui/material/Stack";
+import MUITypography from "@mui/material/Typography";
+import MUICircularProgress from "@mui/material/CircularProgress";
+import SlideCard from "../Carousel/SlideCard";
+import CarouselProfile from "../Carousel/CarouselProfile";
+import useGames from "../../hooks/useGames";
+
+const h3 = {
+  fontSize: {
+    xs: "1.25rem",
+    sm: "2rem",
+    md: "2.5rem",
+  },
+  fontWeight: "500",
+  letterSpacing: "0.5px",
+};
+
+export default function ProfileGamesPlaying() {
+  const { games, isLoading } = useGames("PLAYING");
+
+  return (
+    <MUIStack gap={1}>
+      <MUITypography variant="h3" sx={h3}>
+        Currently Playing
+      </MUITypography>
+
+      {!isLoading ? (
+        games.length > 0 ? (
+          <CarouselProfile
+            slides={games.map((game, index) => {
+              const key = `${game.id}_${game.slug}_${index}}`;
+              return <SlideCard type="games" data={game} key={key} />;
+            })}
+          />
+        ) : (
+          "No Games"
+        )
+      ) : (
+        <MUICircularProgress />
+      )}
+    </MUIStack>
+  );
+}
