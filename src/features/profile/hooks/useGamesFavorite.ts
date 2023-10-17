04c6b902a361e6bfe766c5c9a42c771f57ed8a9b
DikDns <dikdnssocial@gmail.com>
2023-10-17 08:18:38 +0700

    #10 [profile] refactor: useGamesFavorite

---
+++ src/features/profile/hooks/useGamesFavorite.ts
@@ -0,0 +1,54 @@
+"use client";
+
+import { useEffect, useState, useContext } from "react";
+import igdb from "@/lib/igdb";
+import Game from "@/features/games/types/Game";
+import { UserProfileContext } from "../components/UserProfileProvider";
+
+export default function useFavoriteGames() {
+  const user = useContext(UserProfileContext);
+  const [games, setGames] = useState<Game[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    if (user?.gameList?.length === 0) {
+      setIsLoading(false);
+      return;
+    }
+
+    return () => {
+      fetchGames();
+    };
+  }, []);
+
+  const fetchGames = async () => {
+    setIsLoading(true);
+    const fetchLimit = 10;
+    const filteredPlayingGames = [];
+
+    const gameList = user?.gameList
+      ?.filter((game) => game.isFavorited)
+      .sort((a, b) => (a.updatedAt < b.updatedAt ? 1 : -1));
+
+    if (!gameList) return;
+
+    for (let i = 0; i < fetchLimit; i++) {
+      if (gameList[i]) {
+        filteredPlayingGames.push(gameList[i]);
+      }
+    }
+
+    const body = `
+      f name, slug, cover.image_id;
+      l ${fetchLimit};
+      w id=(${filteredPlayingGames.map((game) => game.gameId).join(",")});
+    `;
+
+    const nextGames = await igdb("games", body);
+
+    setGames(nextGames);
+    setIsLoading(false);
+  };
+
+  return { games, isLoading };
+}
