0f1f56233f04660e988457fc7b7c726d0dbae63b
DikDns <dikdnssocial@gmail.com>
2023-10-18 10:26:28 +0700

    #10 fix: h1 and h3 in not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -5,17 +5,40 @@ import { usePathname } from "next/navigation";
 import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Link from "@mui/material/Link";
-import { h3, h5 } from "@/layouts/styles";
 
 export default function NotFound() {
   const pathname = usePathname();
 
   return (
     <Container>
-      <Typography variant="h2" sx={h3} mb={1}>
+      <Typography
+        variant="h2"
+        sx={{
+          fontSize: {
+            xs: "1.25rem",
+            sm: "2rem",
+            md: "2.5rem",
+          },
+          fontWeight: "500",
+          letterSpacing: "0.5px",
+        }}
+        mb={1}
+      >
         Not Found: {pathname}
       </Typography>
-      <Typography component="p" sx={h5} mb={2}>
+      <Typography
+        component="p"
+        sx={{
+          fontSize: {
+            xs: "1rem",
+            sm: "0.75rem",
+            md: "0.75rem",
+          },
+          fontWeight: "500",
+          letterSpacing: "0.5px",
+        }}
+        mb={2}
+      >
         Could not find requested resource
       </Typography>
       <Typography variant="body2">
