a8bcd170d29148e3ea6a0f926f6d6ddbd9f8cf99
DikDns <dikdnssocial@gmail.com>
2023-10-19 13:27:17 +0700

    #10 [games] refactor: GenreTheme type

---
+++ src/features/games/types/GenreTheme.ts
@@ -0,0 +1,11 @@
+type GenreTheme = {
+  id?: string;
+  checksum?: string;
+  created_at?: number;
+  updated_at?: number;
+  name?: string;
+  slug?: string;
+  url?: string;
+};
+
+export default GenreTheme;
