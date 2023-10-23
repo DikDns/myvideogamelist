03ef097769713d68a0d078a6c107408135876235
DikDns <dikdnssocial@gmail.com>
2023-10-23 08:15:53 +0700

    feat: move CarouselMedia to Global Layout

---
+++ src/layouts/Carousel/CarouselMedia.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { ReactNode } from "react";
+import Carousel, { CarouselOptions } from "./Carousel";
+import slideMargin from "./slideMargin";
+
+const carouselMediaOptions: CarouselOptions = {
+  loop: true,
+  align: "start",
+  slidesToScroll: 1,
+  breakpoints: {
+    "(min-width: 600px)": {
+      align: "center",
+      dragFree: true,
+      slidesToScroll: 2,
+    },
+  },
+};
+
+export default function CarouselMedia({ slides }: { slides: ReactNode[] }) {
+  return (
+    <Carousel
+      options={carouselMediaOptions}
+      slides={slides}
+      sxSlide={slideMargin}
+    />
+  );
+}
